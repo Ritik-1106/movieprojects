d50dde9387ebe167ed5712b44ccc395639f74043
Ritik Nagar <ritikn1106@gmail.com>
2024-04-03 21:30:48 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/movieprojects/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.movieprojects
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColorDark: Colors.black,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  Homestate createState() => Homestate();
+}
+
+class Homestate extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("News app"),
+      backgroundColor: Colors.green,
+    ));
+  }
+}
